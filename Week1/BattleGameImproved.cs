1e4c52700cc013ca5013391934cf30225ac5d574
MC <mihail9512@yahoo.co.uk>
2023-04-08 20:29:03 +0200

    ready for submission

---
+++ Week1/BattleGameImproved.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Numerics;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class BattleGameImproved : MonoBehaviour
+{
+    enum GameState { gameStart, stageStart, playerTurn, enemyTurn, gameWon, gameLost }
+    GameState gameState = GameState.gameStart;
+    int roundNumber, stunCtr, playerHP, enemyHP, maxPlayerHP, maxEnemyHP;
+    bool isNegating, isCharging, isStunned, isHealing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxEnemyHP = 30;
+        maxPlayerHP = 15;
+        Debug.Log("Greetings, human! Let the games begin.");
+        gameState = GameState.stageStart;
+        StartStage();
+    }
+    void StartStage()
+    {
+        stunCtr = 0;
+        roundNumber = 1;
+        isNegating = false;
+        isCharging = false;
+        isStunned = false;
+        isHealing = false;
+        playerHP = maxPlayerHP;
+        enemyHP = maxEnemyHP;
+        gameState = GameState.playerTurn;
+        Debug.Log("Your turn, press 1 to ATTACK, 2 to HEAL, 3 to DEFLECT!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameState == GameState.playerTurn) { MakePlayerMove(); }
+        else if (gameState == GameState.enemyTurn) { MakeComputerMove(); }
+        else if (gameState == GameState.gameWon) 
+        {
+            Debug.Log("YOU WON! Congrats! But can you do it again?");
+            StartStage();
+        }
+        else if (gameState == GameState.gameLost)
+        {
+            Debug.Log("YOU DIED. Try Again!");
+            StartStage();
+        }
+    }
+
+    void MakePlayerMove()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) { PlayerAttack(); }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) { PlayerHeal(); }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)) { PlayerDeflect(); }
+    }
+    void PlayerAttack()
+    {
+        int damage = Random.Range(2, 4);
+        if (isStunned) { damage *= 2; }
+        enemyHP -= damage;
+        int heal = Mathf.Min(1, maxPlayerHP - playerHP);
+        playerHP += heal; ;
+        Debug.Log($"You attacked for <color=orange>{damage} damage</color> and gained <color=green>{heal} HP</color> back! <b>Player HP: {playerHP}  Enemy HP: {enemyHP}</b>");
+
+        if (enemyHP <= 0) gameState = GameState.gameWon;
+        else SwitchTurn();
+    }
+    void PlayerHeal()
+    {
+        isHealing = true;
+        Debug.Log($"You charge up a <color=green>heal</color> for next turn. <b>Player HP: {playerHP}</b>");
+        SwitchTurn();
+    }
+    void RestoreHP()
+    {
+        int heal = Mathf.Min(Random.Range(4, 7) + stunCtr, maxPlayerHP - playerHP);
+        playerHP += heal;
+        isHealing = false;
+        Debug.Log($"Your charged up heal restores <color=green>{heal} HP</color>. <b>Player HP: {playerHP}</b>");
+    }
+    void PlayerDeflect()
+    {
+        isNegating = true;
+        SwitchTurn();
+    }
+    void SwitchTurn()
+    {
+        if (gameState == GameState.playerTurn)
+        {
+            gameState = GameState.enemyTurn;
+        } 
+        else if (gameState == GameState.enemyTurn)
+        {
+            gameState = GameState.playerTurn;
+            isNegating = false;
+            roundNumber++;
+            Debug.Log($"<b>Round {roundNumber}!</b> <color=green><b>Player HP: {playerHP}</b></color>  <color=red><b>Enemy HP: {enemyHP}</b></color>");
+            if (isHealing) { RestoreHP(); }
+            Debug.Log("Your turn again! Press 1 to ATTACK, 2 to HEAL, 3 to DEFLECT!");
+        }
+    }
+    void MakeComputerMove()
+    {
+        if (isCharging) { DealChargeDamage(); }
+        if (gameState == GameState.enemyTurn) // Skip this block in case of fatal blow
+        {
+            if (!isStunned)
+            {
+                float diceRoll = Random.value;
+                if (diceRoll < 0.75f) { ComputerAttack(); }
+                else { ComputerCharge(); }
+            }
+            else 
+            {
+                stunCtr += 1;
+                isStunned = false;
+                Debug.Log($"The enemy recovers from the stun. <color=red><b>+{stunCtr} damage to enemy attacks</b></color>");
+                SwitchTurn();
+            }
+        }
+    }
+    void DealChargeDamage()
+    {
+        if (!isNegating)
+        {
+            playerHP = 0;
+            Debug.Log($"You got hit by a charged attack. <b>Player HP: {playerHP}</b>");
+            gameState = GameState.gameLost;
+        }
+        else
+        {
+            Debug.Log($"You negated the charged attack and stunned the opponent! Your heal now restores <color=green><b>+{stunCtr+1} HP</b></color>");
+            isNegating = false;
+            isStunned = true;
+            SwitchTurn();
+        }
+        isCharging = false;
+    }
+    void ComputerAttack()
+    {
+        if (isNegating) 
+        {
+            Debug.Log($"You cant deflect normal attacks, silly!");
+            isNegating = false;
+        }    
+        int damage = Random.Range(2, 6) + stunCtr;
+        playerHP -= damage;
+        Debug.Log($"The enemy hit you for <color=red>{damage} damage</color>! <b>Player HP: {playerHP}</b>");
+
+
+        if (playerHP <= 0) { gameState = GameState.gameLost; }
+        else SwitchTurn();
+    }
+    void ComputerCharge()
+    {
+        Debug.Log("<color=red><b>I am charging a devastating attack!</b></color>"); 
+        isCharging = true;
+        SwitchTurn();
+    }
+}
--- Week1/IntroNotes.md
+++ Week1/IntroNotes.md
@@ -1,6 +1,6 @@
 # Turn-Based Console Battle
 
-## Initial Battle System (BattleGame.cs) <img style="float:right" src="ComputerRNG.png" alt="perfectRng" width="300"/>
+## Initial Battle System <img style="float:right" src="ComputerRNG.png" alt="perfectRng" width="300"/>
 * First impression of the system - if the computer was able to play perfectly, it would only attack. The player has a worse damage output and the heal is only able to stall in the best case (and loses HP in the rest 2/3). So in theory I should lose. 
 * First Playtests - Player feels pretty strong
     * The ability to negate all damage is very powerful, because it also blocks the following attack
@@ -8,17 +8,19 @@
 * If the computer high-rolls you are dead, because of the difference in damage output and insufficient healing. 
 * Almost every game ends up in a state where the player either has around 1-3HP or is dead.
 * Healing is pretty much useless, since it can only heal you for the minimum amount of damage, that the enemy deals
+* You can infinitely chain Blocks
 
 
-## Improvement Ideas (BattleGameImproved.cs)
-* Improve Heal - It now resores between 3 and 5 HP
-* Rework Defend to Deflect - if you deflect a Charge attack, you stun the enemy, causing it to instantly end its turn and skip the next one. Deflecting a normal attack does nothing, otherwise the player can win by spamming this ability and using the free attacks in between stuns.
-* Increase the max damage the computer can deal by 1
-* Increase MaxHP to 12 for both the player and the computer
+## Improvement Ideas
+* Rework Defend to Deflect - if you deflect a Charge attack, you stun the enemy, causing it to instantly end its turn and skip the next one. Deflecting a normal attack does nothing, otherwise the player can win by spamming this ability and using the free attacks in between stuns. Each stun gives +1 damage to the opponent attacks and +1 to the player heal as well.
+* Player attacks deals more damage and also crits stunned opponents. (+100% damage)
+* Increased the the computer damage range (2-5).
+* Increase MaxHP to 15 for both the the player and 30 for the computer.
+* Improve Heal - It now resores between 3 and 6 HP, but resolves on the start of your next turn.
+* Give lifesteal (+1HP) to attack.
 
-
-
-<b>THIS MIGHT MAKE THE PLAYER A BIT OP...</b>
+Might be a bit easy for the player in its current itteration, but it felt fun when I playtested it.
+Could implement a stage system, where the coumputer MaxHP increases, giving the opponent more time to ramp up its damage.
 
 
 
